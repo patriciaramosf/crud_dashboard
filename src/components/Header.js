f3d1e9999e1ee6ff958f894ceb8668130e1a0b36
Patricia Ramos <patriciaramosf94@gmail.com>
2020-07-19 20:29:51 +0100

    main routes structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import Particles from 'react-particles-js';
 const Header = () =>{
 return(
     <div className="header">
         <p>A simple records system using MongoDB, React.js, and Node.js.</p>
-        <p>REST API was implemented on the back-end. Semantic UI React was used for the UI.</p>
+        <p>REST API was implemented on the back-end. Semantic UI React was used for the UI, React Virtualized.</p>
+        <Particles 
+        params={{ 
+          particles: { 
+            number: { 
+              value: 200, 
+              density: { 
+                enable: true, 
+                value_area: 2000, 
+              } 
+            }, 
+          }, 
+        }} 
+      /> 
     </div>
 )
 }
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import CreateUser from './CreateUser';
-import { BrowserRouter as Link } from "react-router-dom";
+/* import CreateUser from './CreateUser'; */
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 const UserList = (props) =>{
+    
 return(
     <div className="UserList">
-        <Link to={props.link}><Button variant="contained" color="primary">
-            Add Info
-        </Button>
+        <Link to={props.link}>
+            <Button variant="contained" color="secondary" className="buttonInfo">
+                Add Info
+            </Button>
         </Link>
     </div>
 )
